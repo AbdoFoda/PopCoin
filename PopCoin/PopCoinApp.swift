d1064a39cfcf5a802553347666123e8dd1047d9f
AbdoFoda <abdofoda@stud.fci-cu.edu.eg>
2025-05-22 17:14:52 +0200

    Initial Commit

---
+++ PopCoin/PopCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopCoinApp.swift
+//  PopCoin
+//
+//  Created by Abdulrahman Foda on 22.05.25.
+//
+
+import SwiftUI
+
+@main
+struct PopCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
