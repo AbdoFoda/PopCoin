25d0109c8f4cdcf4a4e9faffc495b1e6e13d9043
AbdoFoda <abdofoda@stud.fci-cu.edu.eg>
2025-05-22 17:14:52 +0200

    Initial Commit

---
+++ PopCoinTests/PopCoinTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopCoinTests.swift
+//  PopCoinTests
+//
+//  Created by Abdulrahman Foda on 22.05.25.
+//
+
+import Testing
+@testable import PopCoin
+
+struct PopCoinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
